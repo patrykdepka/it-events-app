c5b6c61ca03f602d15d18763879e60cb7e00be7c
Patryk Depka <patrykdepka@gmail.com>
2023-05-18 15:56:41 +0200

    Added custom client-side validation to registration form

--- src/main/resources/static/scripts/validation.js
+++ src/main/resources/static/scripts/validation.js
@@ -7,6 +7,21 @@ function validateLoginForm() {
     return isNotUsernameError && isNotPasswordError;
 }
 
+function validateRegistrationForm() {
+    const firstNameValue = document.registrationForm.firstName.value;
+    const isNotFirstNameError = checkIfNotEmpty('firstName', firstNameValue);
+    const lastNameValue = document.registrationForm.lastName.value;
+    const isNotLastNameError = checkIfNotEmpty('lastName', lastNameValue);
+    const emailValue = document.registrationForm.email.value;
+    const isNotEmailError = checkIfNotEmpty('email', emailValue);
+    const passwordValue = document.registrationForm.password.value;
+    const isNotPasswordError = checkIfNotEmpty('password', passwordValue);
+    const confirmPasswordValue = document.registrationForm.confirmPassword.value;
+    const isNotConfirmPasswordError = checkIfNotEmpty('confirmPassword', confirmPasswordValue);
+
+    return isNotFirstNameError && isNotLastNameError && isNotEmailError && isNotPasswordError && isNotConfirmPasswordError;
+}
+
 function checkIfNotEmpty(fieldName, fieldValue) {
     if (!fieldValue) {
         document.getElementById(`${fieldName}`).className='form-control is-invalid';
