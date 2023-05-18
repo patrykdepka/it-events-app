328460920bc8cb5e2cb89040e570120ea3c5757b
Patryk Depka <patrykdepka@gmail.com>
2023-05-18 14:57:28 +0200

    Created home page and navigation bar

--- src/main/java/pl/patrykdepka/iteventsapp/security/SecurityConfig.java
+++ src/main/java/pl/patrykdepka/iteventsapp/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package pl.patrykdepka.iteventsapp.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -10,9 +11,18 @@ public class SecurityConfig {
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().authenticated();
+        http.authorizeRequests(request -> request
+                .mvcMatchers("/").permitAll()
+                .anyRequest().authenticated());
         http.formLogin();
         http.httpBasic();
         return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> web.ignoring().mvcMatchers(
+                "/styles/**"
+        );
     }
 }
