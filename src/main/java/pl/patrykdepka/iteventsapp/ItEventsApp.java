a4acc2fe98eb364ee8d7839e4d6bb581aebef9b1
Patryk Depka <patrykdepka@gmail.com>
2023-05-18 14:39:20 +0200

    App generated by Spring Initializr

---
+++ src/main/java/pl/patrykdepka/iteventsapp/ItEventsApp.java
@@ -0,0 +1,13 @@
+package pl.patrykdepka.iteventsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItEventsApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItEventsApp.class, args);
+    }
+
+}
---
+++ src/test/java/pl/patrykdepka/iteventsapp/ItEventsAppTests.java
@@ -0,0 +1,13 @@
+package pl.patrykdepka.iteventsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItEventsAppTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
