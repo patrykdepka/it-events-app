7fb9ff6d6b4de929e816a510f0292b091c9efdb2
Patryk Depka <patrykdepka@gmail.com>
2023-05-20 12:17:02 +0200

    Removed User object import

--- src/main/java/pl/patrykdepka/iteventsapp/security/AppUserDetailsServiceImpl.java
+++ src/main/java/pl/patrykdepka/iteventsapp/security/AppUserDetailsServiceImpl.java
@@ -1,7 +1,6 @@
 package pl.patrykdepka.iteventsapp.security;
 
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
